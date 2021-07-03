f0a4dd52b11cb7ddc85e95f268a1454cdf3dad61
ChrisOfNormandy <boobrick@hotmail.com>
2021-07-03 15:33:47 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ConfigGUI } from './components/ConfigGUI';
+
+function App() {
+  return (
+    <div className="App">
+      <ConfigGUI />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ConfigGUI.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ConfigGUI extends React.Component {
+    render() {
+        return <h1>Hello</h1>;
+    }
+}
+
+export { ConfigGUI };
